44a0f90d0d2e6412dd756c99908f3ad4265ea8ba
Daniel KALU <dankalu.work@gmail.com>
2021-02-02 02:20:23 -0800

    Initial Commit

---
+++ PreWork--Daniel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreWork--Daniel
+//
+//  Created by Daniel Kalu on 2/2/21.
+//  Copyright © 2021 daniel KALU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
